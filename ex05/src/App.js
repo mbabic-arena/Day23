eb0450da2cc34e564fca0a704beca9622a7eaf7b
mbabic-arena <m_babic@teol.net>
2021-03-30 23:35:31 +0200

    Forms

---
+++ ex05/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SimpleForm from "./SimpleForm";
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleForm />
+    </div>
+  );
+}
+
+export default App;
