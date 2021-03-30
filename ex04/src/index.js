17cc1263c8901f960a01284e1c959776ec389f18
mbabic-arena <m_babic@teol.net>
2021-03-30 23:40:37 +0200

    State

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -5,8 +5,4 @@ import ReactDOM from "react-dom";
 import App from "./App"; // Add this line
 import "./index.css";
 
-// Remove code below this line
-
-// add the following code below this line
-
 ReactDOM.render(<App />, document.getElementById("root"));
