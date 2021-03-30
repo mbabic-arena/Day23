d5876d665c445f7a6146dca766f64bdd23272d58
mbabic-arena <m_babic@teol.net>
2021-03-30 23:36:06 +0200

    Submitting Form Data

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Add this line
+import "./index.css";
+
+// Remove code below this line
+
+// add the following code below this line
+
+ReactDOM.render(<App />, document.getElementById("root"));
